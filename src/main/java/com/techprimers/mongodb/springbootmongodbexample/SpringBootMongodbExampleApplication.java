191d6991fab23a8c3601a5cb960fa8af1a3f0ba3
apibaque <47745995+apibaque@users.noreply.github.com>
2019-03-03 17:43:00 -0300

    Update SpringBootMongodbExampleApplication.java

--- src/main/java/com/techprimers/mongodb/springbootmongodbexample/SpringBootMongodbExampleApplication.java
+++ src/main/java/com/techprimers/mongodb/springbootmongodbexample/SpringBootMongodbExampleApplication.java
@@ -1,11 +1,14 @@
 package com.techprimers.mongodb.springbootmongodbexample;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
+
+
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class SpringBootMongodbExampleApplication {
 
 	public static void main(String[] args) {
